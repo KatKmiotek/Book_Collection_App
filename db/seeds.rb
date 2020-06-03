6a920afbb4b8d9e1e31226dc3283f43414b6ce36
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2020-06-03 11:14:48 +0100

    css

--- db/seeds.rb
+++ db/seeds.rb
@@ -82,6 +82,69 @@ book8 = Book.new({
   'comment' => '',
   'user_id' => user1.id
 })
+book9 = Book.new({
+  'title' => 'Shakespeare',
+  'author' => 'Bill Bryson',
+  'status' => 'have read',
+  'location' => 'at home',
+  'language' => 'English',
+  'comment' => '',
+  'user_id' => user1.id
+})
+book10 = Book.new({
+  'title' => 'Book of Myth & Legend',
+  'author' => 'J.C. Cooper',
+  'status' => 'reading',
+  'location' => 'at home',
+  'language' => 'English',
+  'comment' => '',
+  'user_id' => user1.id
+})
+book11 = Book.new({
+  'title' => 'The Theatre of the Absurd',
+  'author' => 'Martin Esslin',
+  'status' => 'to read',
+  'location' => 'at home',
+  'language' => 'English',
+  'comment' => '',
+  'user_id' => user1.id
+})
+book12 = Book.new({
+  'title' => 'Boredom',
+  'author' => 'Alberto Moravia',
+  'status' => 'to read',
+  'location' => 'at home',
+  'language' => 'English',
+  'comment' => '',
+  'user_id' => user1.id
+})
+book13 = Book.new({
+  'title' => 'Prowadz swoj plog przez kosci umarlych',
+  'author' => 'Olga Tokarczuk',
+  'status' => 'have read',
+  'location' => 'at home',
+  'language' => 'Polish',
+  'comment' => '',
+  'user_id' => user1.id
+})
+book14 = Book.new({
+  'title' => 'Ulisses',
+  'author' => 'James Joyce',
+  'status' => 'have read',
+  'location' => 'at home',
+  'language' => 'Polish',
+  'comment' => '',
+  'user_id' => user1.id
+})
+book15 = Book.new({
+  'title' => 'Ulysses',
+  'author' => 'James Joyce',
+  'status' => 'to read',
+  'location' => 'at home',
+  'language' => 'English',
+  'comment' => '',
+  'user_id' => user1.id
+})
 book1.save()
 book2.save()
 book3.save()
@@ -90,6 +153,18 @@ book5.save()
 book6.save()
 book7.save()
 book8.save()
+book9.save()
+book10.save()
+book11.save()
+book12.save()
+book13.save()
+book14.save()
+book15.save()
+
+
+
+
+
 
 
 binding.pry
